097b48fe79e0a33aea42bc3dce4fb353f764ad19
kazarka <pavel.marlboro123@gmail.com>
2025-05-25 19:45:58 +0300

    Update elysium_bot.py

--- elysium_bot.py
+++ elysium_bot.py
@@ -102,4 +102,5 @@ def telegram_webhook():
 
 if __name__ == "__main__":
     # запускаем flask
+    port = int(os.getenv("PORT", 5000))  # если не задан, ставим 5000
     app.run(host="0.0.0.0", port=port)
